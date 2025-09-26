8778358a9d364ee5e841b1dee3ff86a28a7944f4
Tymoteusz Szarlej [Home Desktop] <tszarlej@proton.me>
2025-09-27 00:46:16 +0200

    Dodano początkową strukturę projektu i szablony dla RavaDev
    - Utworzono aplikacje _media i _static z niezbędnymi plikami.
    - Dodano style CSS dla układu i komponentów aplikacji.
    - Wdrożono podstawowe szablony HTML dla sekcji „O programie” i „Główna”.
    - Utworzono szablon bazowy dla spójnego układu na wszystkich stronach.
    - Dodano manage.py i start.sh do zarządzania projektem i uruchamiania serwera.

---
+++ RavaDev/RavaDev/urls.py
@@ -0,0 +1,17 @@
+# RavaDev/urls.py - POPRAWNIE
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('Main.urls')),
+    path('about/', include('About.urls')),  # APLIKACJE PRZED STATIC FILES
+    # ... inne aplikacje
+]
+
+# STATIC FILES TYLKO NA KOŃCU
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
